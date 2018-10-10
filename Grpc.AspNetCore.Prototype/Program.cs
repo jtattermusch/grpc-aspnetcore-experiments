4402f6cdb993ebb5a4e8af471ca1965255e06112
Jan Tattermusch <jtattermusch@google.com>
2018-10-10 22:56:58 +0200

    project skeleton

---
+++ Grpc.AspNetCore.Prototype/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+
+namespace Grpc.AspNetCore.Prototype
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureKestrel(options =>
+                {
+                    options.Listen(IPAddress.Any, 8080, listenOptions =>
+                    {
+                        listenOptions.Protocols = HttpProtocols.Http2;
+                    }); 
+                })
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
